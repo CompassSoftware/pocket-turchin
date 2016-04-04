6d10bcb7a7e1f7feedac4fa857f51ac7587f2b92
scott <scottshuffler@gmail.com>
2016-04-04 18:54:42 -0400

    Added a table view

---
+++ PocketTurchin/ArchiveTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ArchiveTableViewCell.swift
+//  PocketTurchin
+//
+//  Created by Owner on 4/4/16.
+//  Copyright © 2016 shuffleres. All rights reserved.
+//
+
+import UIKit
+
+class ArchiveTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
