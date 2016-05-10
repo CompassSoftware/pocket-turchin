cc8df60e74468159e6a5eebc9a6ce7bfb17567c9
scott <scottshuffler@gmail.com>
2016-05-10 16:49:10 -0400

    Working current page

--- PocketTurchin/CurrentTableViewCell.swift
+++ PocketTurchin/CurrentTableViewCell.swift
@@ -2,29 +2,26 @@
 //  CurrentTableViewCell.swift
 //  PocketTurchin
 //
-//  Created by Owner on 4/4/16.
+//  Created by Owner on 5/10/16.
 //  Copyright © 2016 shuffleres. All rights reserved.
 //
 
 import UIKit
 
 class CurrentTableViewCell: UITableViewCell {
-    
+
     @IBOutlet weak var photoImageView: UIImageView!
-    //MARKED PROPERTIES
-    //@IBOutlet weak var archiveLabel: UILabel!
-    //@IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var archiveLabel: UILabel!
-    
     override func awakeFromNib() {
+        
         super.awakeFromNib()
         // Initialization code
     }
-    
+
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-        
+
         // Configure the view for the selected state
     }
-    
+
 }
--- PocketTurchin/CurrentTableViewController.swift
+++ PocketTurchin/CurrentTableViewController.swift
@@ -1,38 +1,29 @@
 //
-//  ArchiveTableViewController.swift
+//  CurrentTableViewController.swift
 //  PocketTurchin
 //
-//  Created by Owner on 4/18/16.
+//  Created by Owner on 5/10/16.
 //  Copyright © 2016 shuffleres. All rights reserved.
 //
 
-
-
 import UIKit
-import Dispatch
-
 
 class CurrentTableViewController: UITableViewController {
     
-    //MARKED PROPERTIES
-    
-    var archives = [Archive]()
+    var currents = [Archive]()
     var photo: UIImage = UIImage(named: "defaultImage")!
     var desc: String = ""
     var name: String = ""
     var archive_count = -1
     var picture_count = 0
     
-    /*
-     Loads all the archives
-     Written horribly
-     Leave me alone
-     */
-    func loadArchives() {
+    func loadAllCurrent() {
+        
+        print("Loading all current")
         self.count_selection { (archive) in
             self.archive_count = archive[0] as! Int
+            print(String(archive[0]))
         }
-        //while self.archive_count == -1 {}
         
         func load_all() {
             self.load_all_archives { (archive) in
@@ -43,7 +34,7 @@ class CurrentTableViewController: UITableViewController {
                         func call_seque()
                         {
                             //self.photo = image!
-                            self.archives[self.picture_count].photo = image!
+                            self.currents[self.picture_count].photo = image!
                             self.picture_count = self.picture_count + 1
                             dispatch_async(dispatch_get_main_queue(), { () -> Void in
                                 self.tableView.reloadData()
@@ -65,119 +56,118 @@ class CurrentTableViewController: UITableViewController {
                 self.name = archive[0] as! String
                 
                 let archive_temp = Archive(name: self.name, desc: self.desc,photo: self.photo)
-                self.archives += [archive_temp]
+                self.currents += [archive_temp]
                 
             }
         }
         dispatch_async(dispatch_get_main_queue(),load_all)
+
     }
-    
-    func log(text: String)
-    {
-        let isMain = NSThread.currentThread().isMainThread
-        let name_of_thread = isMain ? "{Main }" : "{Child}"
-        print("\(name_of_thread) \(text)")
-    }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadArchives()
-        log("Out of the load")
+        loadAllCurrent()
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
     // MARK: - Table view data source
-    
+
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
-    
+
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        //return 0
-        return archives.count
-        
+        return currents.count
     }
-    
+
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        // Table view cells are reused and should be dequeued using a cell identifier.
-        log("Building initial view")
-        let cellIdentifier = "ArchiveTableViewCell"
-        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! ArchiveTableViewCell
+        let cellIdentifier = "CurrentTableViewCell"
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! CurrentTableViewCell
         
         // Fetches the appropriate meal for the data source layout.
-        let archive = archives[indexPath.row]
+        let current = currents[indexPath.row]
         
-        cell.archiveLabel.text = archive.name
-        
-        cell.photoImageView.image = archive.photo
+        cell.archiveLabel.text = current.name
+        cell.photoImageView.image = current.photo
         
         return cell
     }
     
-    
+
     /*
-     // Override to support conditional editing of the table view.
-     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
-    
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
     /*
-     // Override to support editing the table view.
-     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-     if editingStyle == .Delete {
-     // Delete the row from the data source
-     tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-     } else if editingStyle == .Insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
-    
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
     /*
-     // Override to support rearranging the table view.
-     override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-     
-     }
-     */
-    
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
     /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
-    
-    
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
     // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
     
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
         if segue.identifier == "ShowDetail" {
-            let archiveDetailViewController = segue.destinationViewController as! ArchiveViewController
+            let currentDetailViewController = segue.destinationViewController as! ArchiveViewController
             
             // Get the cell that generated this segue.
-            if let selectedArchiveCell = sender as? ArchiveTableViewCell {
-                let indexPath = tableView.indexPathForCell(selectedArchiveCell)!
-                let selectedArchive = archives[indexPath.row]
-                archiveDetailViewController.archive = selectedArchive
+            if let selectedCurrentCell = sender as? CurrentTableViewCell {
+                let indexPath = tableView.indexPathForCell(selectedCurrentCell)!
+                let selectedCurrent = currents[indexPath.row]
+                currentDetailViewController.archive = selectedCurrent
             }
         }
     }
     
     func load_all_archives(completionHandler: (archive: NSArray) -> ()) {
-        let requestURL: NSURL = NSURL(string: "http://cs.appstate.edu/Turchin/archiveGetter.php")!
+        let requestURL: NSURL = NSURL(string: "http://cs.appstate.edu/Turchin/currentGetter.php")!
         let urlRequest: NSMutableURLRequest = NSMutableURLRequest(URL: requestURL)
         let session = NSURLSession.sharedSession()
         let task = session.dataTaskWithRequest(urlRequest) {
@@ -188,6 +178,7 @@ class CurrentTableViewController: UITableViewController {
                 do{
                     let json = try NSJSONSerialization.JSONObjectWithData(data!, options:.AllowFragments)
                     if let datas = json["data"] as? [[String: AnyObject]] {
+                        print(datas)
                         for data in datas {
                             if let name = data["name"] as? String {
                                 if let desc = data["description"] as? String {
@@ -218,7 +209,7 @@ class CurrentTableViewController: UITableViewController {
     }
     
     func count_selection(completionHandler: (archive: NSArray) -> ()) {
-        let requestURL: NSURL = NSURL(string: "http://cs.appstate.edu/Turchin/archiveGetter.php")!
+        let requestURL: NSURL = NSURL(string: "http://cs.appstate.edu/Turchin/currentGetter.php")!
         let urlRequest: NSMutableURLRequest = NSMutableURLRequest(URL: requestURL)
         let session = NSURLSession.sharedSession()
         let task = session.dataTaskWithRequest(urlRequest) {
@@ -255,9 +246,14 @@ class CurrentTableViewController: UITableViewController {
             }
             //completion(image: UIImage(data: data!))
         }
-        
         task.resume()
     }
     
-    
+    func log(text: String)
+        {
+            let isMain = NSThread.currentThread().isMainThread
+            let name_of_thread = isMain ? "{Main }" : "{Child}"
+            print("\(name_of_thread) \(text)")
+        }
+
 }
